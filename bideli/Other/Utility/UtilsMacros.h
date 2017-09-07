7ff4b9995be90da48513626ae2368defb23a42ff
Naruto-yq <yuqinyanglijuan@icloud.com>
2017-09-08 00:58:36 +0800

    解决mm文件编译报错问题

--- bideli/Other/Utility/UtilsMacros.h
+++ bideli/Other/Utility/UtilsMacros.h
@@ -78,9 +78,9 @@
 #define INCREASE_COLOR HEXCOLOR(0xff602c)
 #define DECREASE_COLOR HEXCOLOR(0x76ca74)
 
-#define random(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)/255.0]
+#define randomRGB(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)/255.0]
 
-#define randomColor random(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
+#define randomColor randomRGB(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
 
 //通知消息
 #define  ZZLCQQShareEndNotification       @"ZZLCQQShareEndNotification"
